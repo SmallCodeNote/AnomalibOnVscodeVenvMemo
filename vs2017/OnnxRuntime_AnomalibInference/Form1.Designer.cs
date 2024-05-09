8ef2e376969593af6ecb87c33c97228e4516d1a4
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-05-10 06:54:23 +0900

    Add files via upload

---
+++ vs2017/OnnxRuntime_AnomalibInference/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace OnnxRuntime_AnomalibInference
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Run = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.textBox_ImageFilesPath = new System.Windows.Forms.TextBox();
+            this.button_LoadImageFiles = new System.Windows.Forms.Button();
+            this.textBox_OnnxFilePath = new System.Windows.Forms.TextBox();
+            this.button_LoadOnnxFilePath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_Run
+            // 
+            this.button_Run.Location = new System.Drawing.Point(8, 241);
+            this.button_Run.Name = "button_Run";
+            this.button_Run.Size = new System.Drawing.Size(75, 23);
+            this.button_Run.TabIndex = 11;
+            this.button_Run.Text = "Run";
+            this.button_Run.UseVisualStyleBackColor = true;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "ImageFiles";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Onnx File";
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Location = new System.Drawing.Point(33, 270);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_Result.Size = new System.Drawing.Size(456, 286);
+            this.textBox_Result.TabIndex = 6;
+            this.textBox_Result.WordWrap = false;
+            // 
+            // textBox_ImageFilesPath
+            // 
+            this.textBox_ImageFilesPath.Location = new System.Drawing.Point(33, 94);
+            this.textBox_ImageFilesPath.Multiline = true;
+            this.textBox_ImageFilesPath.Name = "textBox_ImageFilesPath";
+            this.textBox_ImageFilesPath.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_ImageFilesPath.Size = new System.Drawing.Size(456, 126);
+            this.textBox_ImageFilesPath.TabIndex = 7;
+            this.textBox_ImageFilesPath.WordWrap = false;
+            // 
+            // button_LoadImageFiles
+            // 
+            this.button_LoadImageFiles.Location = new System.Drawing.Point(8, 94);
+            this.button_LoadImageFiles.Name = "button_LoadImageFiles";
+            this.button_LoadImageFiles.Size = new System.Drawing.Size(23, 21);
+            this.button_LoadImageFiles.TabIndex = 4;
+            this.button_LoadImageFiles.Text = "...";
+            this.button_LoadImageFiles.UseVisualStyleBackColor = true;
+            this.button_LoadImageFiles.Click += new System.EventHandler(this.button_LoadImageFiles_Click);
+            // 
+            // textBox_OnnxFilePath
+            // 
+            this.textBox_OnnxFilePath.Location = new System.Drawing.Point(33, 27);
+            this.textBox_OnnxFilePath.Name = "textBox_OnnxFilePath";
+            this.textBox_OnnxFilePath.Size = new System.Drawing.Size(456, 19);
+            this.textBox_OnnxFilePath.TabIndex = 8;
+            // 
+            // button_LoadOnnxFilePath
+            // 
+            this.button_LoadOnnxFilePath.Location = new System.Drawing.Point(8, 27);
+            this.button_LoadOnnxFilePath.Name = "button_LoadOnnxFilePath";
+            this.button_LoadOnnxFilePath.Size = new System.Drawing.Size(23, 21);
+            this.button_LoadOnnxFilePath.TabIndex = 5;
+            this.button_LoadOnnxFilePath.Text = "...";
+            this.button_LoadOnnxFilePath.UseVisualStyleBackColor = true;
+            this.button_LoadOnnxFilePath.Click += new System.EventHandler(this.button_LoadOnnxFilePath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 588);
+            this.Controls.Add(this.button_Run);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.textBox_ImageFilesPath);
+            this.Controls.Add(this.button_LoadImageFiles);
+            this.Controls.Add(this.textBox_OnnxFilePath);
+            this.Controls.Add(this.button_LoadOnnxFilePath);
+            this.Name = "Form1";
+            this.Text = "OnnxRuntime_AnomalibInference";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Run;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.TextBox textBox_ImageFilesPath;
+        private System.Windows.Forms.Button button_LoadImageFiles;
+        private System.Windows.Forms.TextBox textBox_OnnxFilePath;
+        private System.Windows.Forms.Button button_LoadOnnxFilePath;
+    }
+}
+
---
+++ vs2017/OnnxRuntime_AnomalibInference/OnnxImageClassificationLoader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenCvSharp;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace OnnxRuntime_ImageClassification
+{
+    class OnnxImageClassification
+    {
+        static public string RunSession(string onnxFilePath, string imageFilePath, bool ImShow = false)
+        {
+            Mat imgSrc = Cv2.ImRead(imageFilePath, ImreadModes.Color);
+
+            int inputWidth = imgSrc.Width;
+            int inputHeight = imgSrc.Height;
+            string dstString = RunSessionAndDrawMat(onnxFilePath, imgSrc, ImShow);
+            imgSrc.Dispose();
+
+            return dstString;
+        }
+
+        static public string RunSessionAndDrawMat(string onnxFilePath, Mat imgSrc, bool ImShow = false)
+        {
+            using (var session = new InferenceSession(onnxFilePath))
+            {
+                return RunSessionAndDrawMat(session, imgSrc, ImShow);
+            }
+        }
+
+        static public string RunSessionAndDrawMat(InferenceSession session, Mat imgSrc,  bool ImShow = false)
+        {
+            int inputWidth = imgSrc.Width;
+            int inputHeight = imgSrc.Height;
+            var input = getDenseTensorFromMat(imgSrc, inputWidth, inputHeight);
+
+            var inputShape = new DenseTensor<float>(new[] { 1, 2 });
+            inputShape[0, 0] = imgSrc.Height;
+            inputShape[0, 1] = imgSrc.Width;
+
+            var inputMeta = session.InputMetadata;
+            var inputName = inputMeta.First().Key;
+            var inputDims = inputMeta.First().Value.Dimensions;
+
+            var inputs = new NamedOnnxValue[] { NamedOnnxValue.CreateFromTensor(inputName, input) };
+
+            List<string> LineOutput = new List<string>();
+
+            using (var results = session.Run(inputs))
+            {
+                Tensor<float> scores = results[1].AsTensor<float>();
+
+                // Process results
+                int indicesLength = (int)scores.Length;
+                for (int i = 0; i < indicesLength; i++)
+                {
+                    var score = scores[0];
+                    LineOutput.Add(score.ToString("g4"));
+                }
+
+
+                Tensor<float> segmentationImage = results[0].AsTensor<float>();
+
+                // Tensorの次元を取得（高さ、幅）
+                int height = 224;
+                int width = 224;
+
+                // 新しいMatを作成
+                Mat grayscaleImage = new Mat(height, width, MatType.CV_8UC1);
+
+                // Tensorの各要素を走査
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+
+                            // Tensorからピクセル値を取得
+                            float pixelValue = segmentationImage[0,0,0,y*width + x];
+
+                            // ピクセル値を0-255の範囲にスケーリング
+                            byte grayScale = (byte)(pixelValue * 2);
+
+                            // Matに新しいピクセル値を設定
+                            grayscaleImage.Set(y, x, grayScale);
+
+                    }
+                }
+
+                // Matを保存
+                Cv2.ImWrite(@"R:\result\grayscaleImage.png", grayscaleImage);
+            }
+
+            return string.Join("\t", LineOutput.Take(3).ToArray());
+        }
+
+        static private DenseTensor<float> getDenseTensorFromMat(Mat src, int tensorWidth, int tensorHeight)
+        {
+            var dstTensor = new DenseTensor<float>(new[] { 1, 3, tensorWidth, tensorHeight });
+
+            Size newSize = new Size(tensorWidth, tensorHeight);
+            Mat dst = new Mat();
+            Cv2.Resize(src, dst, newSize);
+
+            for (int y = 0; y < tensorHeight; y++)
+            {
+                for (int x = 0; x < tensorWidth; x++)
+                {
+                    Vec3b color = dst.At<Vec3b>(y, x);
+                    dstTensor[0, 0, y, x] = ((float)color.Item2) / 255f;
+                    dstTensor[0, 1, y, x] = ((float)color.Item1) / 255f;
+                    dstTensor[0, 2, y, x] = ((float)color.Item0) / 255f;
+                }
+            }
+
+            dst.Dispose();
+            return dstTensor;
+        }
+
+    }
+}
